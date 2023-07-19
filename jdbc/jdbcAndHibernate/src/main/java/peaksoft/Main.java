544b3f88766897078a988a89fcd4eb0650a99e8d
AdiletAskaraliev <adilet.askaraliev17@gmail.com>
2023-07-19 21:40:58 +0600

    done project

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
@@ -1,7 +1,29 @@
 package peaksoft;
 
+import com.sun.xml.bind.v2.runtime.unmarshaller.UnmarshallingContext;
+import peaksoft.model.User;
+import peaksoft.service.UserService;
+import peaksoft.service.UserServiceImpl;
+import peaksoft.util.Util;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+//        Util.getConnection();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Alex", "Morgan", (byte) 28);
+        userService.saveUser("Asan", "Asanov", (byte) 17);
+        userService.saveUser("Uson", "Usonov", (byte) 20);
+        userService.saveUser("Ivan", "Ivanov", (byte) 23);
+        userService.getAllUsers().forEach(System.out::println);
+        userService.removeUserById(5);
+        userService.getAllUsers().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.getAllUsers().forEach(System.out::println);
+        userService.dropUsersTable();
+
+
+
     }
 }
--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/dao/UserDaoJdbcImpl.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/dao/UserDaoJdbcImpl.java
@@ -1,7 +1,10 @@
 package peaksoft.dao;
 
 import peaksoft.model.User;
+import peaksoft.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJdbcImpl implements UserDao {
@@ -12,25 +15,110 @@ public class UserDaoJdbcImpl implements UserDao {
 
     public void createUsersTable() {
 
+        String sql = "create table if not exists users(" +
+                "id serial primary key," +
+                "name varchar," +
+                "lastName varchar," +
+                "age smallint)";
+
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            statement.executeUpdate(sql);
+
+            System.out.println("Table successfully created");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void dropUsersTable() {
+        String sql = "drop table users";
 
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            statement.executeUpdate(sql);
+
+            System.out.println("Table successfully deleted");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String sql = "insert into users(" +
+                "name, lastName, age)" +
+                "values (?, ?, ?)";
 
+        try(Connection connection = Util.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setInt(3, age);
+            preparedStatement.executeUpdate();
+            System.out.println(name + " is saved");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void removeUserById(long id) {
+        String sql = "delete from users where id = ?";
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
+            preparedStatement.setLong(1, id);
+            int rowsUpdated = preparedStatement.executeUpdate();
+
+            if (rowsUpdated > 0) {
+                System.out.println(id + " is deleted");
+            } else {
+                System.out.println("No student with ID " + id + " found.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+
+        String sql = "select * from users";
+
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                users.add(new User(
+                        resultSet.getLong("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("lastname"),
+                        resultSet.getByte("age")
+                ));
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return users;
+
     }
 
     public void cleanUsersTable() {
+        String sql = "delete from users";
 
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.executeUpdate();
+
+            System.out.println("Students was cleaned");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
